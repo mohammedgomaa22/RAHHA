16f47d758cf0e2ede64ba599e88f62e044ed66f3
Moahmmed Gomaa <mohammedgomaa2522@gmail.com>
2025-02-01 17:06:18 +0200

    upload landing

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#000000',
+      },
+
+    },
+  },
+  plugins: [],
+}
